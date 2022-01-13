f42516baec71722511fd34c00cc97db8fada373a
sungwookang <sungwookang11@gmail.com>
2022-01-14 03:10:17 +0900

    new url

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="w_container">
     <span>{{weather}}</span><br>
-    <span>{{city}}</span><br>
-    <span>{{temper}}</span>
+    <span>{{temper}}</span><br>
   </div>
   
 </template>
 
 <script>
-const API_KEY = "20d3ea9ebdb399b0edf375ee7d5085c0";
+const API_KEY = "40fdd308ac2317c9f38271955e6f8e0d";
 
 export default {
   name: 'App',
   data() {
     return {
      weather: "",
-     city: "",
+
      temper: "",
 
     }
@@ -27,10 +26,7 @@ export default {
     geoGood(position) {
       const lat = position.coords.latitude; //위도 설정
       const lon = position.coords.longitude; //경도 설정
-      const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}&units=metric`;
-                //16daily = api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}
-                //one call = https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily&appid=${API key}
-                //original = https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
+      let url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}&units=metric`;
       fetch(url)
           .then(response => response.json())//여기도 추가공부...
           .then(data => {
